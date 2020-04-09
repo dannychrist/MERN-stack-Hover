92b7cb8c381de26205e911ee800557db5fcc7c3b
Eric Han <eric_h94@hotmail.com>
2020-04-09 17:46:00 -0400

    router-link-commit

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom'
+
+//components
+import GlobalStyles from '../components/GlobalStyles/GlobalStyles'
+import Header from '../components/Header/header'
 
 function App() {
   const [bacon, setBacon] = useState(null);
@@ -9,7 +18,20 @@ function App() {
       .then(data => setBacon(data));
   }, []);
 
-  return <div>{bacon ? bacon : `...where's my stuff?...`}</div>;
+  return (
+    <Router>
+      <div style={{padding: '0 2.5% 0 2.5%'}}>
+        <Header/>
+        <Switch>
+          <Route exact path='/'>Home</Route>
+          <Route exact path='/shop'>Shop</Route>
+          <Route exact path='/shop/:id'>Shop with filter</Route>
+          <Route exact path='/product/:productId'>Single item</Route>
+          <Route exact path='/cart'>cart</Route>
+        </Switch>
+      </div>
+      <GlobalStyles />
+    </Router>
+  )
 }
-
 export default App;
